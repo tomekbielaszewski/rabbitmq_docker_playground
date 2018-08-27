9c37f604c167e58cb9625b868d67217639052f9a
tbielaszewski <tomekbielaszewski@gmail.com>
2018-08-27 16:19:49 +0200

    Dockerized Java helloworld

---
+++ hello_world/receiver/src/main/java/pl/grizwold/rabbitmq_tutorial/Receiver.java
@@ -0,0 +1,8 @@
+package pl.grizwold.rabbitmq_tutorial;
+
+public class Receiver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ hello_world/sender/src/main/java/pl/grizwold/rabbitmq_tutorial/Sender.java
@@ -0,0 +1,8 @@
+package pl.grizwold.rabbitmq_tutorial;
+
+public class Sender {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
